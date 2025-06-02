49e1e1bd648f1a50d80a662753f65e9a4626a43a
MeoKool <truongsyquang1202@gmail.com>
2025-06-02 16:25:50 +0700

    Add error handling middleware and configure Express app

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Server is up and running");
 });
+
+app.use(errorHandler);
 
 module.exports = app;
